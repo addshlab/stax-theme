a694156f6d7c0870dff4269bc8a84ebaa76bd62a
hackasassets <nakad@add.sh>
2018-01-23 12:10:32 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,121 @@
+<?php
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 600;
+}
+
+/**
+ * テーマにコンポーネントされていないファイルの検知
+ */
+function no_component_files() {
+	$message = '<p>Stax Themeの基本構成ファイルは functions.php, index.php, screenshot.png, style.css の4つです。</p>';
+	$flag = 0;
+	$theme_dir_path = get_stylesheet_directory();
+	$allow_files = array(
+		$theme_dir_path . '/functions.php',
+		$theme_dir_path . '/index.php',
+		$theme_dir_path . '/screenshot.png',
+		$theme_dir_path . '/style.css',
+	);
+
+	foreach ( glob( $theme_dir_path . '/*', GLOB_BRACE ) as $file_path ) {
+		if( is_dir( $file_path ) ){
+			if ( false === in_array( $file_path, $allow_files ) ) {
+				$dir_alert .= '<p>テーマディレクトリ内にコンポーネント外のディレクトリ ' . $file_path . ' が存在します。</p>';
+				$flag = 1;
+			}
+		}
+		if( is_file( $file_path ) ){
+			if ( false === in_array( $file_path, $allow_files ) ) {
+				$file_alert .= '<p>テーマディレクトリ内にコンポーネント外のファイル ' . $file_path . ' が存在します。</p>';
+				$flag = 1;
+			}
+		}
+	}
+
+	if ( $flag === 1 ) {
+		echo '<div class="message error">';
+		echo $dir_alert;
+		echo $file_alert;
+		echo $message;
+		echo '</div>';
+	}
+}
+add_action ( 'admin_notices', 'no_component_files' );
+
+/**
+ * 管理画面以外でもDashiconsを使用する
+ * DashiconsはWordPress3.8以上で使用可能なアイコンフォントセットです
+ * @see https://developer.wordpress.org/resource/dashicons/
+ */
+function enqueue_scripts() {
+    wp_enqueue_style( 'dashicons', site_url( '/' ) . '/wp-includes/css/dashicons.min.css' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+
+/**
+ * style.cssにファイルタイムスタンプ由来のフィンガープリントを付与
+ * @see https://worklog.be/archives/2983
+*/
+function default_style_fingerprint( $styles ) {
+	$mtime = filemtime( get_stylesheet_directory() . '/style.css' );
+	$styles->default_version = $mtime;
+}
+add_action( 'wp_default_styles', 'default_style_fingerprint' );
+
+/**
+ * テーマのサポートする機能の定義
+ */
+function custom_theme_setup() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'automatic-feed-links' );
+}
+add_action( 'after_setup_theme', 'custom_theme_setup' );
+
+/**
+ * タイトルタグのカスタマイズ
+ */
+function wpdocs_hack_wp_title_for_home( $title ) {
+	if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+		$title = get_bloginfo( 'name' );
+	} elseif ( is_single() ) {
+		$title = get_the_title() . ' | ' . get_bloginfo( 'name' );
+	} else {
+		$title = get_the_title();
+	}
+	return $title;
+}
+add_filter( 'pre_get_document_title', 'wpdocs_hack_wp_title_for_home' );
+
+/**
+ * Meta Description のカスタマイズ
+ */
+ function add_meta_description() {
+ 	if ( is_home() ) {
+		$content = get_bloginfo( 'description' );
+	} elseif ( is_single() ) {
+		$content = get_the_excerpt();
+	} else {
+		$content = '';
+	}
+
+ 	echo '<meta name="description" content="' . $content . '">';
+ }
+ add_filter( 'wp_head', 'add_meta_description' );
+
+/**
+ * ウィジェットエリアの定義
+ */
+function theme_slug_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Footer menu(Stax Theme)', 'stax' ),
+		'id' => 'footer-menu',
+		'description' => __( 'Display menu to footer.', 'stax' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'theme_slug_widgets_init' );
+
---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * HTML header
+ * header.phpに分割可能
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head profile="http://gmpg.org/xfn/11">
+		<meta charset=<?php bloginfo('charset'); ?>>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+<?php wp_head(); ?>
+	</head>
+<body <?php body_class(); ?>>
+
+<?php
+/**
+ * グローバルヘッダ
+ * header.phpに分割可能
+ * トップページはH1, それ以外ではPタグでマークアップ
+ */
+?>
+
+<header class="global">
+	<?php if ( is_home() || is_front_page() ) : ?>
+	<h1>
+	<?php else : ?>
+	<p>
+	<?php endif; ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+	<?php if ( is_home() || is_front_page() ) : ?>
+	</h1>
+	<?php else : ?>
+	</p>
+	<?php endif; ?>
+</header>
+<?php
+/**
+ * コンテンツエリア
+ * 構造の共通度に応じて以下のテンプレートに分割可能
+ * トップページ: front-page.php > home.php > index.php
+ * アーカイブ: category.php > archive.php > index.php
+ * 検索: search.php > index.php
+ */
+?>
+<main>
+
+<?php if ( is_404() ) : ?>
+		<article>
+			<h1 class="title">404 Page Not Found</h1>
+		</article>
+<?php elseif ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+			<header>
+<?php
+/**
+ * 投稿タイトルの表示
+ * 投稿ページと固定ページではH1, それ以外ではH2でマークアップ
+ */
+?>
+				<?php if ( is_single() || is_page() ) : ?>
+				<time datetime="<?php echo get_the_time( 'Y-m-d' ); ?>">
+					<?php the_time( 'Y-m-d' ); ?>
+				</time>
+				<h1 class="title">
+					<?php if ( mb_strlen( get_the_title() ) === 0 ) : echo '(no title)' ; else : the_title(); endif; ?>
+				</h1>
+				<?php else : ?>
+				<time datetime="<?php echo get_the_time( 'Y-m-d' ); ?>">
+					<?php the_time( 'Y-m-d' ); ?>
+				</time>
+				<h2 class="title">
+					<a href="<?php the_permalink(); ?>">
+						<?php echo mb_substr( get_the_title(), 0, 100 ); ?><?php if ( mb_strlen( get_the_title() ) === 0 ) : echo '(no title)' ; elseif ( mb_strlen( get_the_title() ) >= 100 ) : echo ' ...'; endif; ?>
+					</a>
+				</h2>
+				<?php endif; ?>
+
+			</header>
+<?php
+/**
+ * 投稿内容の表示
+ * 投稿ページと固定ページで表示
+ */
+?>
+			<?php if ( is_single() || is_page() ) : ?>
+			<?php the_content(); ?>
+			<?php wp_link_pages(); ?>
+			<?php the_tags(); ?>
+			<?php else : ?>
+			<div class="excerpt">
+					<?php echo mb_substr( strip_tags( get_the_content() ), 0, 150 ); ?><?php if ( mb_strlen( strip_tags( get_the_content() ) ) === 0 ) : echo '(no content)' ; elseif ( mb_strlen( strip_tags( get_the_content() ) ) >= 150 ) : echo ' ...'; endif; ?>
+				</div>
+			<?php endif; ?>
+<?php
+/**
+ * コメントの投稿とコメントのリストを表示
+ * 投稿ページと固定ページで表示
+ */
+?>
+			<?php if ( is_single() || is_page() ) : ?>
+			<ol class="commentlist">
+			<?php wp_list_comments(); ?>
+			</ol>
+			<?php comments_template(); ?>
+			<?php paginate_comments_links(); ?>
+			<?php endif; ?>
+
+		</article>
+
+<?php endwhile; ?>
+<?php else: ?>
+<?php endif; ?>
+
+	<nav class="paginate_links">
+		<?php echo paginate_links( array(
+			'show_all' => true,
+		) ); ?>
+	</nav>
+
+</main>
+<?php
+/**
+ * グローバルフッタ
+ * 一般的にfooter.phpに含められる
+ */
+?>
+<footer>
+	<nav>
+
+<?php if ( is_active_sidebar( 'footer-menu' ) ) : ?>
+	<div id="footer-menu">
+		<?php dynamic_sidebar( 'footer-menu' ); ?>
+	</div>
+<?php endif; ?>
+
+	</nav>
+</footer>
+	
+<?php wp_footer(); ?>
+
+</body>
+</html>
